3dbae0472fcd5882642453972e125b77bb6eea75
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-09-24 20:54:39 -0300

    Configurando Mapping

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server:127.0.0.1;Database=dbAPI;Uid=sa;Pwd=123456";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new MyContext(optionsBuilder.Options);
+
+        }
+    }
+}
