86169f0b555f671e3bdfe392fe5962f4e7c260aa
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-01 16:09:08 -0300

    Implementando AutoMapper

---
+++ src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain.DTO.User;
+using Domain.Models.User;
+
+namespace CrossCutting.Mappings
+{
+    public class DtoToModelProfile : Profile
+    {
+        public DtoToModelProfile()
+        {
+            CreateMap<UserModel, UserDTO>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Domain.DTO.User;
+using Domain.Entities;
+
+namespace CrossCutting.Mappings
+{
+    public class EntityToDtoProfile : Profile 
+    {
+        public EntityToDtoProfile()
+        {
+            CreateMap<UserDTO, UserEntity>()
+                .ReverseMap();
+
+            CreateMap<UserCreateResultDTO, UserEntity>()
+                .ReverseMap();
+
+            CreateMap<UserUpdateResultDTO, UserEntity>()
+                .ReverseMap();
+        }
+    }
+}
