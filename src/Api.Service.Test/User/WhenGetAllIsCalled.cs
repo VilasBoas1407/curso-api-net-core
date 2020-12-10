93d7094db7197d7df8a39a18cbc3fbcc147d51ee
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-10 16:46:28 -0300

    Criando classe de teste ,para testar o método de GetAll()

---
+++ src/Api.Service.Test/User/WhenGetAllIsCalled.cs
@@ -0,0 +1,41 @@
+﻿using Domain.DTO.User;
+using Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+    public class WhenGetAllIsCalled : UserTest
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+
+        [Fact(DisplayName = "É possível executar o método GET All")]
+        public async Task E_possivel_Executar_Metodo_GetAll()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(listaUserDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.GetAll();
+            Assert.NotNull(result);
+            Assert.True(result.Count() == 10);
+
+            var _listResult = new List<UserDTO>();
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(_listResult);
+            _service = _serviceMock.Object;
+
+            var resultEmpty = await _service.GetAll();
+            Assert.Empty(resultEmpty);
+            Assert.True(resultEmpty.Count() == 0);
+        }
+
+    }
+}
