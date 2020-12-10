9ae0e3d99a34f946868cd595a1eef096c7fd746b
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-10 16:33:42 -0300

    Classe de Teste Quando o método Get é chamado

---
+++ src/Api.Service.Test/User/WhenGetIsCalled.cs
@@ -0,0 +1,42 @@
+﻿using Domain.DTO.User;
+using Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+    public class WhenGetIsCalled : UserTest
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName ="É possível executar o método GET")]
+        public async Task E_Possivel_Executar_Metodo_GET()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Get(IdUsuario)).ReturnsAsync(userDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Get(IdUsuario);
+            
+            Assert.NotNull(result);
+            Assert.True(result.Id == IdUsuario);
+            Assert.Equal(NomeUsuario, result.Name);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Get(It.IsAny<Guid>())).Returns(Task.FromResult((UserDTO)null));
+            _service = _serviceMock.Object;
+
+            var record = await _service.Get(IdUsuario);
+            Assert.Null(record);
+    
+        }
+        
+
+
+    }
+}
