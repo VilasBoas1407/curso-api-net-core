b6e22ad6c1384c0df8591b5ee8aee44f143250b6
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-10 17:00:57 -0300

    Classe de teste quando o método Create é chamado. Será que não podia colocar tudo em um único arquivo de teste não ? Verificar depois

---
+++ src/Api.Service.Test/User/WhenCreateIsCalled.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces.Services.User;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+    public class WhenCreateIsCalled : UserTest
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName ="É possível executar o Método Create")]
+        public async Task E_Possivel_Executar_Metodo_Create()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Post(userDTOCreate)).ReturnsAsync(userCreateResult);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Post(userDTOCreate);
+            Assert.NotNull(result);
+            Assert.Equal(NomeUsuario, result.Name);
+            Assert.Equal(EmailUsuario, result.Email);
+        }
+    }
+}
