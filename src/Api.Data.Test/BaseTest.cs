74df95179dd48569d7c759c2f1de4481052b0eb5
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-11 11:18:23 -0300

    Update

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using Data.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+}
