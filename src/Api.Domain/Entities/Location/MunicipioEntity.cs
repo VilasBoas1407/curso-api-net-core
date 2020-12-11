553b95101da4cafe1c4ab1d2145ab439e54671e0
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-11 16:59:51 -0300

    Criando Classes Uf e Municipio

---
+++ src/Api.Domain/Entities/Location/MunicipioEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Entities.Location
+{
+    public class MunicipioEntity : BaseEntity
+    {
+        public string Nome { get; set; }
+        public int CodIBGE { get; set; }
+        public Guid UfId { get; set; }
+        public UfEntity Uf { get; set; }
+    }
+}
