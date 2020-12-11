3e5012780b02cf87f13a7d4b02ddc4f8f973e592
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-11 11:42:52 -0300

    Testes na classe de Data

--- src/Api.Data.Test/BaseTest.cs
+++ src/Api.Data.Test/BaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using Data.Context;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,6 +12,35 @@ namespace Api.Data.Test
         public BaseTest()
         {
 
+        }
+    }
+
+        public class DbTeste : IDisposable
+    {
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseSqlServer($"Persist Security Info=True;Server=localhost;Database={dataBaseName};User=sa;Password=123456"),
+                  ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
         }
     }
 }
