98e487c86ac03af1b341e6be3fced5540473eda4
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-14 14:21:23 -0300

    Adicionando Migrations

--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -1,5 +1,7 @@
 ﻿using Data.Mapping;
+using Data.Seeds;
 using Domain.Entities;
+using Domain.Entities.Location;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,16 +9,32 @@ using System.Text;
 
 namespace Data.Context
 {
-    public class MyContext: DbContext
+    public class MyContext : DbContext
     {
         public DbSet<UserEntity> Users { get; set; }
 
-        public MyContext(DbContextOptions<MyContext> options) : base(options) {}
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+            modelBuilder.Entity<UfEntity>(new UfMap().Configure);
+            modelBuilder.Entity<MunicipioEntity>(new MunicipioMap().Configure);
+            modelBuilder.Entity<CepEntity>(new CepMap().Configure);
+
+            modelBuilder.Entity<UserEntity>().HasData(
+                new UserEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Administrador",
+                    Email = "lucas.vilas@email.com",
+                    CreateAt = DateTime.Now,
+                    UpdateAt = DateTime.Now,
+                }
+            );
+
+            UfSeeds.Ufs(modelBuilder);
         }
 
     }
---
+++ src/Api.Data/Mapping/UfMap.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Mapping
+{
+    public class UfMap : IEntityTypeConfiguration<UfEntity>
+    {
+        public void Configure(EntityTypeBuilder<UfEntity> builder)
+        {
+            builder.ToTable("UF");
+
+            builder.HasKey(u => u.Id);
+
+            builder.HasIndex(u => u.Sigla)
+                   .IsUnique();
+
+        }
+    }
+}
