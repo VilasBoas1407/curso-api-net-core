4156b05f91e348414b17d50ae9a79cc05ec7929f
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-12-15 14:39:32 -0300

    Adicionando classes de teste UF

---
+++ src/Api.Service.Test/UF/UfTestes.cs
@@ -0,0 +1,43 @@
+﻿using Domain.DTO.Location.UF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Test.UF
+{
+    public class UfTestes
+    {
+        public static string Nome { get; set; }
+        public static string Sigla { get; set; }
+        public static Guid IdUf { get; set; }
+
+        public List<UfDTO> listaUfDto = new List<UfDTO>();
+        public UfDTO ufDto;
+
+        public UfTestes()
+        {
+            IdUf = Guid.NewGuid();
+            Sigla = Faker.Address.UsState().Substring(1, 3);
+            Nome = Faker.Address.UsState();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var dto = new UfDTO()
+                {
+                    Id = Guid.NewGuid(),
+                    Sigla = Faker.Address.UsState().Substring(1, 3),
+                    Nome = Faker.Address.UsState()
+                };
+                listaUfDto.Add(dto);
+            };
+
+            ufDto = new UfDTO
+            {
+                Id = IdUf,
+                Sigla = Sigla,
+                Nome = Nome
+            };
+        }
+
+    }
+}
